b51c9a58b0df55ced6b2a9959d3faf421d373205
BHH <brucehenryhayes@yahoo.com>
2020-10-09 18:21:06 -0400

    hw4 push 1

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+var questions = [
+    {
+        title: "Commonly Used data types DO NOT include ______.",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+    },
+
+    {
+        title: "The condition of an if/else statement is enclosed within ______.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
+    },
+
+    {
+        title: "Arrays in JavaScript can be used to store ______.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+
+    {
+        title: "String values must be enclosed within ______ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        answer: "3. quotes"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is ______.",
+        choices: ["1. JavaScipt", "2. terminal / bash", "3. for loops", "4. console.log"],
+        answer: "4. console.log"
+    }
+
+]
+
+
+// var questionEl = document.querySelector(".question")
+// var answerEl = document.querySelector(".answer")
+
+// var Quiz = [{question}]
+
+
+
+// //click
+// answerEl.addEventListener("click", function(e)({ e.preventDefault();
+//     var target1 = e.target;
+
+
+
+// }
