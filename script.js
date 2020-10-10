63bcd64f76fde813bde7aa4e7d8261a08e5e4994
BHH <brucehenryhayes@yahoo.com>
2020-10-10 16:45:08 -0400

    hw4 push2

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-
-
+// var startPage = document.getElementById("startPage");
+var qstnField = document.getElementById("qstnField");
+var timer = document.getElementById("timer");
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
+var results = document.getElementById("results");
+var nameInitials = document.getElementById("nameInitials");
+var initials = document.getElementById("initials");
+var submitBtn = document.getElementById("submitBtn");
+var scores = document.getElementById("scoresThang");
 var questions = [
     {
         title: "Commonly Used data types DO NOT include ______.",
@@ -30,8 +38,91 @@ var questions = [
         choices: ["1. JavaScipt", "2. terminal / bash", "3. for loops", "4. console.log"],
         answer: "4. console.log"
     }
-
 ]
+
+var Q = 0
+
+startBtn.addEventListener("click", startGame);
+// submitBtn.addEventListener("click", storeInitial);
+
+function startGame() {
+    var startScreen = document.getElementById("startPage");
+    startScreen.setAttribute("class", "hide");
+    qstnField.removeAttribute("class", "hide");
+    buildQuestion();
+
+    timerID = setInterval(runClock, 1000);
+}
+
+function buildQuestion() {
+    var questionEl = document.getElementById("question");
+    var answersEl = document.getElementById("answers");
+
+    questionEl.textContent = questionArray[Q].question;
+
+    answersEl.innerHTML = "";
+
+    questionArray[Q].choices.array.forEach(function (choice) {
+        var button = document.createElement("button");
+        button.setAttribute("value", choice);
+
+        button.textContent = choice;
+        button.setAttribute("class", "btn");
+
+        button.onclick = selectAnswer;
+
+        answersEl.appendChild(button);
+    });
+}
+
+function selectAnswer() {
+    console.log(this.value);
+
+    if (this.value !== questionArray[Q].answer) {
+        console.log("wrong");
+        time -= 10;
+        this.classList.add("wrong");
+        console.log(this);
+
+    } else {
+        this.classlist.add("correct");
+        console.log("correct");
+        time += 10;
+        score++
+    }
+    Q++;
+    if (Q === questionArray.length) {
+        setTimeout(endGame, 500);
+    } else {
+        setTimeout(buildQuestion, 500);
+    }
+}
+
+function endGame() {
+    qstnField.setAttribute("class", "hide");
+    clearInterval(timerID);
+    results.classlist.remove("hide");
+}
+
+function runClock() {
+    time--;
+    var timer = document.getElementById("timer");
+    timer.textContent = time;
+    console.log(time);
+
+    if (time <= 0) {
+        setTimeout(endGame, 500);
+    }
+}
+
+function storeInitials(e) {
+    e.preventDefault();
+    var initStart = intitFinish.value;
+    console.log(initStart);
+    console.log(score);
+
+}
+
 
 
 // var questionEl = document.querySelector(".question")
